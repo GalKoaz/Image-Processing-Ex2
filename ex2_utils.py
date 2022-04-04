6d6f1072697e477794668580cba2e178f097b7ef
Gal <koazgal@gmail.com>
2022-04-04 21:41:51 +0300

    Update Files

---
+++ ex2_utils.py
@@ -0,0 +1,104 @@
+import math
+import numpy as np
+import cv2
+
+
+def conv1D(in_signal: np.ndarray, k_size: np.ndarray) -> np.ndarray:
+    """
+    Convolve a 1-D array with a given kernel
+    :param in_signal: 1-D array
+    :param k_size: 1-D array as a kernel
+    :return: The convolved array
+    """
+
+    return
+
+
+def conv2D(in_image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    """
+    Convolve a 2-D array with a given kernel
+    :param in_image: 2D image
+    :param kernel: A kernel
+    :return: The convolved image
+    """
+
+    return
+
+
+def convDerivative(in_image: np.ndarray) -> (np.ndarray, np.ndarray):
+    """
+    Calculate gradient of an image
+    :param in_image: Grayscale iamge
+    :return: (directions, magnitude)
+    """
+
+    return
+
+
+def blurImage1(in_image: np.ndarray, k_size: int) -> np.ndarray:
+    """
+    Blur an image using a Gaussian kernel
+    :param in_image: Input image
+    :param k_size: Kernel size
+    :return: The Blurred image
+    """
+
+    return
+
+
+def blurImage2(in_image: np.ndarray, k_size: int) -> np.ndarray:
+    """
+    Blur an image using a Gaussian kernel using OpenCV built-in functions
+    :param in_image: Input image
+    :param k_size: Kernel size
+    :return: The Blurred image
+    """
+
+    return
+
+
+def edgeDetectionZeroCrossingSimple(img: np.ndarray) -> np.ndarray:
+    """
+    Detecting edges using "ZeroCrossing" method
+    :param img: Input image
+    :return: opencv solution, my implementation
+    """
+
+    return
+
+
+def edgeDetectionZeroCrossingLOG(img: np.ndarray) -> np.ndarray:
+    """
+    Detecting edges usint "ZeroCrossingLOG" method
+    :param img: Input image
+    :return: opencv solution, my implementation
+    """
+
+    return
+
+
+def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
+    """
+    Find Circles in an image using a Hough Transform algorithm extension
+    To find Edges you can Use OpenCV function: cv2.Canny
+    :param img: Input image
+    :param min_radius: Minimum circle radius
+    :param max_radius: Maximum circle radius
+    :return: A list containing the detected circles,
+                [(x,y,radius),(x,y,radius),...]
+    """
+
+    return
+
+
+def bilateral_filter_implement(in_image: np.ndarray, k_size: int, sigma_color: float, sigma_space: float) -> (
+        np.ndarray, np.ndarray):
+    """
+    :param in_image: input image
+    :param k_size: Kernel size
+    :param sigma_color: represents the filter sigma in the color space.
+    :param sigma_space: represents the filter sigma in the coordinate.
+    :return: OpenCV implementation, my implementation
+    """
+
+    return
